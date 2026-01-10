33bcc2ccbdde6df0c945824d3c366f0c6f3c279a
mrdiess <65924943+mrdiess@users.noreply.github.com>
2026-01-10 23:28:19 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-      },
-    ],
+    domains: ["lh3.googleusercontent.com"],
   },
 }
 
