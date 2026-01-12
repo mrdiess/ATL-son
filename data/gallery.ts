db7f19f1801f7664eeba434377899ea5ee54bc2d
mrdiess <65924943+mrdiess@users.noreply.github.com>
2026-01-12 14:39:26 +0300

    a

--- data/gallery.ts
+++ data/gallery.ts
@@ -7,12 +7,12 @@ export type GalleryItem = {
 export const gallery: GalleryItem[] = [
   {
     id: "1",
-    src: "https://drive.google.com/uc?export=view&id=1ZzX-UsEC1r9AeaBODtwadEFQL2Soek4-",
+    src: "https://lh3.googleusercontent.com/d/1ZzX-UsEC1r9AeaBODtwadEFQL2Soek4-",
     category: "ferforje",
   },
   {
     id: "2",
-    src: "https://drive.google.com/uc?export=view&id=1eCBetPTxXI9oBhd8gv9y_THE0AoeBF3T",
+    src: "https://lh3.googleusercontent.com/d/1eCBetPTxXI9oBhd8gv9y_THE0AoeBF3T",
     category: "merdiven",
   },
 ]
